7f0b4d31731a42e818eaf8b00b1a271343b1f909
tordvb10 <tordvb10.kodehode@gmail.com>
2024-05-21 16:45:42 +0200

    vs

--- BuberBreakfast/obj/Release/net8.0/BuberBreakfast.AssemblyInfo.cs
+++ BuberBreakfast/obj/Release/net8.0/BuberBreakfast.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberBreakfast")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+076c94e732417aae4c48f7fa9400b3c6405b1842")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberBreakfast")]
